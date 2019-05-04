380398f5219eb820001f541425ff07adefd7112e
Germán Muñoz <dataficado@gmail.com>
2019-05-03 21:31:01 -0500

    Se incluye setup.py para empaquetar.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="banrep",
+    version="0.0.1",
+    description="Funciones y rutinas para BanRep",
+    author="Germán Muñoz",
+    author_email="gmunozbr@banrep.gov.co",
+    packages=["banrep"],
+)
