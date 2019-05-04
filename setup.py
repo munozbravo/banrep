9965a19a3641a6c5b20de2f936b33959945ee04a
Germán Muñoz <dataficado@gmail.com>
2019-05-04 11:34:46 -0500

    Actualiza setup.py con find_packages y README

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_one = fh.read()
 
 setup(
     name="banrep",
     version="0.0.1",
     description="Funciones y rutinas para BanRep",
-    author="Germán Muñoz",
-    author_email="gmunozbr@banrep.gov.co",
-    packages=["banrep"],
+    long_description=long_one,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munozbravo/banrep",
+    author="dataficado",
+    author_email="dataficado@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7.3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
