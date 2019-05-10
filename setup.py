e57b8bf7576bc590e7c7503afd69d2bdb9654fc6
Germán Muñoz <dataficado@gmail.com>
2019-05-10 17:24:16 -0500

    Se usa __version__.py para valores en setup.

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from setuptools import setup, find_packages
 
 
-with open("README.md", "r", encoding='utf-8') as fh:
-    long_one = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+about = {}
+with open("__version__.py", "r", encoding="utf-8") as f:
+    exec(f.read(), about)
 
 setup(
-    name="banrep",
-    version="0.0.1",
-    description="Funciones y rutinas para BanRep",
-    long_description=long_one,
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/munozbravo/banrep",
-    author="Germán Muñoz",
-    author_email="gamunozbravo@gmail.com",
-    packages=find_packages(exclude=['docs', 'tests']),
+    url=about["__url__"],
+    author=about["__author__"],
+    author_email=about["__email__"],
+    packages=find_packages(exclude=["docs", "tests"]),
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='~=3.7',
+    python_requires="~=3.7",
     include_package_data=True,
-    install_requires=[
-        "spacy>=2.1.3",
-        "tika>=1.19"
-    ]
+    install_requires=["spacy>=2.1.3", "tika>=1.19"],
 )
