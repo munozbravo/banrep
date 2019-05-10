7859b1d015f3e5318bbae24a18e5089cf9642bf7
Germán Muñoz <dataficado@gmail.com>
2019-05-10 17:23:01 -0500

    Se incluye __version__.py para usar en setup.

---
+++ banrep/__version__.py
@@ -0,0 +1,10 @@
+#
+__title__ = "banrep"
+__version__ = "0.0.1"
+__description__ = "Análisis de texto en Banco de la República"
+__url__ = "https://github.com/munozbravo/banrep"
+__author__ = "Germán Muñoz"
+__email__ = "gamunozbravo@gmail.com"
+__license__ = "MIT"
+
+__download_url__ = "https://github.com/munozbravo/banrep"
