846787a51946d5f79d46d1733634e21c8d41b6f2
Germán Muñoz <gamunozbravo@gmail.com>
2019-05-18 15:04:54 -0500

    leer_texto, iterar_textos y guardar_texto probado.

--- banrep/io.py
+++ banrep/io.py
@@ -2,13 +2,15 @@
 """Módulo para funciones de lectura y escritura."""
 from pathlib import Path
 
+from banrep.utils import iterar_rutas
+
 
 def leer_texto(archivo):
     """Lee texto de un archivo.
 
     Parameters
     ----------
-    archivo: str|Path
+    archivo : str | Path
         Ruta del archivo del cual se quiere leer texto.
 
     Returns
@@ -21,16 +23,36 @@ def leer_texto(archivo):
     if ruta.is_file():
         try:
             with open(ruta, encoding="utf-8") as f:
-                text = f.read()
+                texto = f.read()
 
         except Exception:
             print(f"No pudo extraerse información de {ruta.name}.")
+            texto = ""
 
     else:
         print(f"{ruta.name} no es un archivo.")
-        text = ""
+        texto = ""
 
-    return text
+    return texto
+
+
+def iterar_textos(directorio, aleatorio=False):
+    """Itera sobre cada ruta en directorio y extrae el texto.
+
+    Parameters
+    ----------
+    directorio: str | Path
+        Directorio a iterar.
+    aleatorio : bool
+        Iterar aleatoriamente.
+
+    Yields
+    ------
+    str
+        Texto de archivo en ruta.
+    """
+    for archivo in iterar_rutas(directorio, aleatorio=aleatorio):
+        yield leer_texto(archivo)
 
 
 def guardar_texto(texto, archivo):
--- banrep/tests/test_io.py
+++ banrep/tests/test_io.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """Modulo para pruebas de io."""
 
-from banrep.io import guardar_texto, leer_texto
+from banrep.io import guardar_texto, leer_texto, iterar_textos
 
 import pytest
 
@@ -28,3 +28,27 @@ def test_leer_texto(tmp_path):
 def test_leer_texto_arg0():
         with pytest.raises(TypeError):
                 texto = leer_texto()
+
+
+def test_iterar_textos(tmp_path):
+        texto = "Hello"
+        archivo = tmp_path.joinpath("filas.txt")
+        guardar_texto(texto, archivo)
+
+        iterable = iterar_textos(tmp_path)
+        assert len(list(iterable)) == 1
+
+
+def test_iterar_textos_aleatorios(tmp_path):
+        for i in range(10):
+                texto = f"Hello{i}"
+                archivo = tmp_path.joinpath(f"filas{i}.txt")
+                guardar_texto(texto, archivo)
+
+        aleatorios = iterar_textos(tmp_path, aleatorio=True)
+
+        assert list(aleatorios) != list(iterar_textos(tmp_path))
+
+def test_iterar_textos_arg0():
+        with pytest.raises(TypeError):
+                iterable = iterar_textos()
