727944d9dd5fa74c07fc0da81d9538731949ba74
Germán Muñoz <gamunozbravo@gmail.com>
2019-05-18 14:38:56 -0500

    iterar_rutas y crear_directorio con pruebas.

--- banrep/utils.py
+++ banrep/utils.py
@@ -1,6 +1,7 @@
 # coding: utf-8
-"""Módulo para variables y funciones de uso comun."""
+"""Módulo para funciones de interacción con el sistema."""
 from pathlib import Path
+import random
 
 
 def crear_directorio(nombre):
@@ -10,7 +11,7 @@ def crear_directorio(nombre):
 
     Parameters
     -------------
-    nombre : str
+    nombre : str | Path
         Nombre de nuevo directorio a crear.
 
     Returns
@@ -24,3 +25,28 @@ def crear_directorio(nombre):
         ruta.mkdir(parents=True, exist_ok=True)
 
     return ruta
+
+
+def iterar_rutas(directorio, aleatorio=False):
+    """Itera rutas de archivos en directorio, en orden o aleatoriamente.
+
+    Parameters
+    ----------
+    directorio : str | Path
+        Directorio a iterar.
+    aleatorio : bool
+        Iterar aleatoriamente.
+
+    Yields
+    ------
+    Path
+        Ruta de archivo.
+    """
+    absoluto = Path(directorio).resolve()
+    rutas = (ruta for ruta in absoluto.iterdir() if ruta.is_file())
+
+    todas = sorted(rutas)
+    if aleatorio:
+        random.shuffle(todas)
+
+    yield from todas
