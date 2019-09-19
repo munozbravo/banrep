a6e5ba275a330ce7dbcf2f69c2fba1b2b8795b13
Germán Muñoz <gamunozbravo@gmail.com>
2019-09-19 11:20:05 -0500

    Agrega fn de preprocesamiento.
    unir_fragmentos, eliminar_newlines, separar_guiones, separar_numeros

--- banrep/preprocesos.py
+++ banrep/preprocesos.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 """Módulo para funciones de preprocesamiento de texto."""
+import re
 
 
 def filtrar_cortas(texto, chars=0):
@@ -24,3 +25,67 @@ def filtrar_cortas(texto, chars=0):
 
     return filtrado
 
+
+def unir_fragmentos(texto):
+    """Une fragmentos de palabras partidas por final de línea.
+
+    Parameters
+    ----------
+    texto : str
+
+    Returns
+    -------
+    str
+        Texto con palabras de fin de línea unidas si estaban partidas.
+    """
+    # Asume ord('-') == 45
+    return texto.replace('-\n', '').replace('-\r\n', '')
+
+
+def eliminar_newlines(texto):
+    """Elimina caracteres de fin de línea.
+
+    Parameters
+    ----------
+    texto : str
+
+    Returns
+    -------
+    str
+        Texto sin caracteres de fin de línea.
+    """
+    return ' '.join(texto.split())
+
+
+def separar_guiones(texto):
+    """Separa guiones de primera y última palabra de fragmentos de texto.
+
+    Parameters
+    ----------
+    texto : str
+
+    Returns
+    -------
+    str
+        Texto con guiones de fragmentos separados de las palabras.
+    """
+    # Asume ord('–') == 8211
+    nuevo = re.sub(r'(\W)–([A-Za-z]+)', r'\1– \2', texto)
+    nuevo = re.sub(r'([A-Za-z]+)–(\W)', r'\1 –\2', nuevo)
+
+    return nuevo
+
+
+def separar_numeros(texto):
+    """Separa números de palabras que los tienen.
+
+    Parameters
+    ----------
+    texto : str
+
+    Returns
+    -------
+    str
+        Texto con números separados de palabras.
+    """
+    return re.sub(r'([A-Za-z]{2,}?)(\d+)', r'\1 \2', texto)
