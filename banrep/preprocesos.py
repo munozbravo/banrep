fd644a8be03f9bbc80a155365a4161cd4f6b2f0b
Germán Muñoz <gamunozbravo@gmail.com>
2019-09-20 12:46:52 -0500

    Incluye fn eliminar_chars y limpiar_extraccion.

--- banrep/preprocesos.py
+++ banrep/preprocesos.py
@@ -3,6 +3,26 @@
 import re
 
 
+def eliminar_chars(texto, basura=None):
+    """Elimina caracteres en texto.
+
+    Parameters
+    ----------
+    texto : str
+    basura : str | list
+        Caracteres a eliminar.
+
+    Returns
+    -------
+    str
+       Texto sin caracteres.
+    """
+    if basura:
+        texto = re.sub(f"[{''.join(basura)}]", '', texto)
+
+    return texto
+
+
 def filtrar_cortas(texto, chars=0):
     """Filtra líneas en texto de longitud chars o inferior.
 
@@ -90,3 +110,30 @@ def separar_numeros(texto):
         Texto con números separados de palabras.
     """
     return re.sub(r'([A-Za-z]{2,}?)(\d+)', r'\1 \2', texto)
+
+
+def limpiar_extraccion(texto, basura=None, chars=0):
+    """Limpieza de texto extraido.
+
+    Parameters
+    ----------
+    texto : str
+    basura : str | list
+        Caracteres a eliminar.
+    chars : int
+        Mínimo número de caracteres en una línea de texto.
+
+    Returns
+    -------
+    str
+       Texto procesado.
+    """
+    limpio = eliminar_chars(texto, basura=basura)
+    limpio = filtrar_cortas(limpio, chars=chars)
+    if limpio:
+        limpio = unir_fragmentos(limpio)
+        limpio = eliminar_newlines(limpio)
+        limpio = separar_guiones(limpio)
+        limpio = separar_numeros(limpio)
+
+    return limpio
