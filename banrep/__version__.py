ec55b1b392d7b0ea96c8bfb88b423f236ec422f5
Germán Muñoz <gamunozbravo@gmail.com>
2020-04-10 14:53:35 -0500

    Version 6.0.1.

--- banrep/__version__.py
+++ banrep/__version__.py
@@ -1,6 +1,6 @@
 #
 __title__ = "banrep"
-__version__ = "6.0.0"
+__version__ = "6.0.1"
 __description__ = "Analítica de Texto en el Banco de la República"
 __url__ = "https://github.com/munozbravo/banrep"
 __author__ = "Germán Muñoz"
