0e85584a1d3277d98fbe922cda8fe35fefe3740b
Germán Muñoz <gamunozbravo@gmail.com>
2020-04-09 20:50:34 -0500

    Cambia df_palabras a df_tokens, incluye ngrams.

--- banrep/tabulados.py
+++ banrep/tabulados.py
@@ -4,22 +4,28 @@
 import pandas as pd
 
 
-def df_palabras(docs):
-    """Crea DataFrame de palabras en corpus.
+def df_tokens(frases, ng=False):
+    """DataFrame de tokens en frases.
 
     Parameters
     ----------
-    docs : Iterable[dict (text: str, tokens: list, meta: dict)]
-        Anotaciones lingüísticas de cada frase.
+    frases : Iterable[dict (text: str, tokens: list[dict | str], meta: dict)]
+        Anotaciones lingüísticas o Info de cada frase.
+    ng : Bool
+        Si frases contienen n-gramas.
 
     Returns
     -------
     pd.DataFrame
-        Anotaciones lingüísticas de cada palabra.
+        Anotaciones lingüísticas o Info de cada token.
     """
     dfs = []
-    for frase in docs:
-        df = pd.DataFrame(frase.get("tokens"))
+    for frase in frases:
+        if ng:
+            df = pd.DataFrame({"token": frase.get("tokens")})
+        else:
+            df = pd.DataFrame(frase.get("tokens"))
+
         for k, v in frase.get("meta").items():
             df[k] = v
 
@@ -28,12 +34,12 @@ def df_palabras(docs):
     return pd.concat(dfs, ignore_index=True)
 
 
-def df_frases(docs):
-    """Crea DataFrame de frases en corpus.
+def df_frases(frases):
+    """DataFrame de frases.
 
     Parameters
     ----------
-    docs : Iterable[dict (text: str, tokens: list, meta: dict)]
+    frases : Iterable[dict (text: str, tokens: list[dict | str], meta: dict)]
         Anotaciones lingüísticas de cada frase.
 
     Returns
@@ -41,34 +47,10 @@ def df_frases(docs):
     pd.DataFrame
         Texto y Metadata de cada frase.
     """
-    df = pd.DataFrame(frase.get("meta") for frase in docs)
-    df["text"] = [frase.get("text") for frase in docs]
+    df = pd.DataFrame(frase.get("meta") for frase in frases)
+    df["text"] = [frase.get("text") for frase in frases]
 
     return df
-
-
-def df_ngramas(ngramed):
-    """Crea DataFrame de n-gramas en corpus.
-
-    Parameters
-    ----------
-    ngramed : Iterable[tuple (list, dict (text: str, tokens: list, meta: dict))]
-        Palabras y n-gramas de cada frase, y Anotaciones lingüísticas.
-
-    Returns
-    -------
-    pd.DataFrame
-        N-gramas y metadata de frase.
-    """
-    dfs = []
-    for tokens, frase in ngramed:
-        df = pd.DataFrame({"token": tokens})
-        for k, v in frase.get("meta").items():
-            df[k] = v
-
-        dfs.append(df)
-
-    return pd.concat(dfs, ignore_index=True)
 
 
 def df_probables(modelo, n=15):
